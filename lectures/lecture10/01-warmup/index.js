aa33ab7c5d41d0c535f6fa5991d7b54927531954
--global <tcurran@unca.edu>
2022-09-21 12:33:36 +0000

    added lecture 10

---
+++ lectures/lecture10/01-warmup/index.js
@@ -0,0 +1,17 @@
+const toggleFall = () => {
+    /*
+        Your task: target the body element and change it's class to "fall"
+    */
+}
+
+const toggleWinter = () => {
+    /*
+        Your task: target the body element and change it's class to "winter"
+    */
+}
+
+const toggleSpring = () => {
+    /*
+        Your task: target the body element and change it's class to "spring"
+    */
+}
