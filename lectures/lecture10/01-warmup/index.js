91922d61b066bf09cf3b671e1be36d0c4fde2575
--global <tcurran@unca.edu>
2022-09-21 12:42:33 +0000

    example one done

--- lectures/lecture10/01-warmup/index.js
+++ lectures/lecture10/01-warmup/index.js
@@ -2,16 +2,19 @@ const toggleFall = () => {
     /*
         Your task: target the body element and change it's class to "fall"
     */
+   document.querySelector("body").className = "fall";
 }
 
 const toggleWinter = () => {
     /*
         Your task: target the body element and change it's class to "winter"
     */
+   document.querySelector("body").className = "winter";
 }
 
 const toggleSpring = () => {
     /*
         Your task: target the body element and change it's class to "spring"
     */
+        document.querySelector("body").className = "spring";
 }
