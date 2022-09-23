8b7138fcafa2605bd8733e39d589fcc79df9630f
--global <tcurran@unca.edu>
2022-09-23 12:26:14 +0000

    calculator

--- lectures/lecture10/05-make-a-calculator/index.js
+++ lectures/lecture10/05-make-a-calculator/index.js
@@ -2,12 +2,20 @@ const addNumbers = (ev) => {
     // Your job: 
     // 1. Access the first number that the user typed in and store it
     //    as a variable.
+    let firstnNumber = document.querySelector("#num1").value;
+    firstnNumber = Number(firstnNumber) 
     // 2. Access the first number that the user typed in and store it
     //    as a variable.
+    let secondNumber = document.querySelector("#num2").value;
+    secondNumber = Number(secondNumber)
+    let sum = firstnNumber + secondNumber;
+
+    document.querySelector("#answer").innerHTML = sum;
     // 3. Convert the values stored inside of the two variables to a 
     //    number (so that you can do math)!
     // 4. Add the two numbers together.
     // 5. Write the result to the #answer section
+
 }
 
 const subtractNumbers = (ev) => {
