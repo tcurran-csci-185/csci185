74915ac7d137e93af7a45b4d68682c67dcb5e4ba
tom <tcurran@unca.edu>
2022-09-23 18:43:18 -0400

    added semi working  dyslexia mode

--- tutorials/tutorial06/03-dyslexia-mode/index.js
+++ tutorials/tutorial06/03-dyslexia-mode/index.js
@@ -2,3 +2,7 @@
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
 */
+
+const dyslexiaMode = ev => {
+  document.querySelector("body").className = "dyslexia-mode"
+}
