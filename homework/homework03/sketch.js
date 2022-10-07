de44da43438b0d69d6788ea66e42a469432cb44c
tom <tcurran@unca.edu>
2022-10-07 19:04:22 -0400

    homework03, good enough for now + some more playing around

--- homework/homework03/sketch.js
+++ homework/homework03/sketch.js
@@ -9,57 +9,65 @@ function setup() {
     // invoke drawCreature 5 times with the following arguments
     // (feel free to change the sizes, positions, and colors):
     
-    drawCreature(93, 113, 166, 'black', 'white');
-    drawCreature(483, 113, 233, '#5e47a7', '#90a747');
-    drawCreature(409, 403, 433, 'white', 'black');
-    drawCreature(333, 223, 133, '#e2d72b', '#7b2be2');
-    drawCreature(113, 313, 99, 'white', 'black');
+    drawCreature(709, 413, 166, 'black', 'white');
+    drawCreature(683, 13, 233, '#800080', '#008080');
+    drawCreature(209, 403, 433, 'white', 'black');
+    drawCreature(523, 223, 133, 'hotpink', '#b469ff');
+    drawCreature(573, 113, 99, 'white', 'black');
+    drawCreature(9, 999, 19, 'white', 'black');
+    drawCreature(109, 999, 29, 'white', 'black');
+    drawCreature(209, 999, 39, 'white', 'black');
+    drawCreature(309, 999, 49, 'white', 'black');
     drawCreature(409, 999, 59, 'white', 'black');
     drawCreature(509, 999, 69, 'white', 'black');
     drawCreature(609, 999, 79, 'white', 'black');
     drawCreature(709, 999, 89, 'white', 'black');
     drawCreature(809, 999, 99, 'white', 'black');
-    drawCreature(909, 313, 89, 'white', 'black');
+    drawCreature(709, 303, 89, 'white', 'black');
     drawCreature(409, 899, 79, 'white', 'black');
     drawCreature(509, 899, 69, 'white', 'black');
     drawCreature(609, 899, 59, 'white', 'black');
     drawCreature(709, 899, 49, 'white', 'black');
     drawCreature(809, 899, 39, 'white', 'black');
+    drawCreature(909, 899, 29, 'white', 'black');
+    drawCreature(1009, 899, 19, 'white', 'black');
+    drawCreature(1109, 899, 9, 'white', 'black');
+    drawCreature(1209, 899, -1, 'white', 'black');
     // drawGrid(canvasWidth, canvasHeight);
 }
 
 // define your drawCreature function here:
 
 // Why does it seem like everything has a border now??
-// probably a much smaeter way to do the calculations...
+// probably a much smarter way to do the calculations...
 function drawCreature(centerX, centerY, size, primaryColor='black', secondaryColor='white') {
     fill('grey')
     square(centerX + size * .09, centerY + size  * .09, size * 1.02); 
     
     fill(primaryColor);
-    square( centerX + size * .10, centerY + size * .10, size); 
+    square( centerX + size * .1, centerY + size * .1, size); 
     
     fill(secondaryColor);
-    rect( centerX + size * .10, centerY + size * .10, size * .50 , size); 
+    rect( centerX + size * .1, centerY + size * .1, size * .5 , size); 
     
     fill(primaryColor);
-    rect( centerX + size * .38, centerY + size * .25 , size * .02, size * .10); 
+    rect( centerX + size * .38, centerY + size * .25 , size * .02, size * .1); 
     
     fill(secondaryColor);
-    rect(centerX + size * .86, centerY + size * .25 , size * .02, size * .10); 
+    rect(centerX + size * .86, centerY + size * .25 , size * .02, size * .1); 
     
     fill(secondaryColor );
-    triangle(centerX + size * .60, centerY +size * .30, centerX + size * .60 , 
-    centerY +size * .60, centerX + size * .70, centerY + size * .60); 
+    triangle(centerX + size * .6, centerY +size * .3, centerX + size * .6 , 
+    centerY +size * .6, centerX + size * .7, centerY + size * .6); 
 
     fill(primaryColor);
     rect(centerX + size * .42, centerY + size * .78, size * .18, size * .02); 
     
     fill(secondaryColor);
-    rect(centerX + size * .60, centerY + size * .78, size * .18, size * .02); 
+    rect(centerX + size * .6, centerY + size * .78, size * .18, size * .02); 
     
     fill(primaryColor);
-    rect(centerX + size * .32, centerY + size, size * .78, size * .10); 
+    rect(centerX + size * .32, centerY + size, size * .78, size * .1); 
 
 }
 
