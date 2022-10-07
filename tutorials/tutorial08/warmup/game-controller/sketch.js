f246db24587e07b290b7a2b027c6ea5233ad7e2f
--global <tcurran@unca.edu>
2022-10-07 13:20:40 -0400

    moving the circle around

--- tutorials/tutorial08/warmup/game-controller/sketch.js
+++ tutorials/tutorial08/warmup/game-controller/sketch.js
@@ -1,6 +1,6 @@
 let x = 100;
 let y = 200;
-let width = 50;
+let width = 100;
 let fillColor = 'white';
 
 const canvasWidth = window.innerWidth;
@@ -21,13 +21,29 @@ const moveController = ev => {
     // TODO:
     // left arrow moves circle left
     // right arrow moves circle right
-    // up arrow moves circle up
     // down arrow moves circle down
 
+     // up arrow moves circle up
+    if(ev.code == 'ArrowUp') {
+        y = y - 10;
+    } else if (ev.code == 'ArrowDown') {
+        y = y + 10;
+    } else if (ev.code == 'ArrowLeft') {
+        x = x - 10;
+    } else if (ev.code == 'ArrowRight') {
+        x = x + 10;
+    } else if (ev.code == 'KeyR') {
+        fillColor = 'red'
+    } else if (ev.code == 'KeyP') {
+        fillColor = 'purple'
+    // } else if (ev.code == 'ArrowRight' + 'ArrowUp' ) {
+    //     x = x - 10;
+    //     y = y + 10;
+    }
 
+    fill(fillColor);
     // redraw circle:
     clear();
-    noFill();
     circle(x, y, width);
     drawGrid(canvasWidth, canvasHeight);
 }
