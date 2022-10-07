9094a0a7a00e10d461667ea538891e4e4defcf95
--global <tcurran@unca.edu>
2022-10-07 13:45:16 -0400

    played with variables

--- tutorials/tutorial08/warmup/ball/sketch.js
+++ tutorials/tutorial08/warmup/ball/sketch.js
@@ -7,15 +7,18 @@ function setup() {
 
 let x = 200;
 let speed = 5;
+let width = 50
 
 function draw() {
-    frameRate(30);
+    frameRate(10);
     clear();
-// when the ball hits the right wall, make it bounce
+// when the ball hits the right wall, make it bounce...
     if (x >= 475) {
-        speed = (speed + 10) * -1;
+        speed = (speed + 1)* -1;
+        width = (width + 2)
     } else if (x <= 175) {
-        speed = (-1 * speed + 10);
+        speed = (-1 * speed + 1);
+        width = (width - 1)
     }
 
     // draw walls:
@@ -25,7 +28,7 @@ function draw() {
 
     // draw circle:
     fill('black');
-    circle(x, canvasHeight/2, 50);
+    circle(x, canvasHeight/2, width);
     x += speed;
     console.log("x position is:", x);
 
