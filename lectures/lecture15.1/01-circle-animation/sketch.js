2ca73a2a1110e8b744fa4560eb290c14497b0c33
--global <tcurran@unca.edu>
2022-10-19 13:20:22 -0400

    redoing lecture 15 ball

--- lectures/lecture15.1/01-circle-animation/sketch.js
+++ lectures/lecture15.1/01-circle-animation/sketch.js
@@ -1,11 +1,4 @@
-// const circleData = [
-//     {x: 500, y: 50, d: 100, speedX: 3, speedY: 3},
-//     {x: 300, y: 150, d: 50, speedX: 2, speedY: 0},
-//     {x: 400, y: 400, d: 25, speedX: 0, speedY: 1},
-//     {x: 40, y: 520, d: 80, speedX: 2, speedY: 0},
-//     {x: 140, y: 120, d: 150, speedX: 0, speedY: -0.5},
-//     {x: 350, y: 350, d: 70, speedX: 1, speedY: 1}
-// ];
+
 
 const canvasWidth = window.innerWidth;
 const canvasHeight = window.innerHeight; 
@@ -16,19 +9,60 @@ function setup() {
 }
 
 // initialize variables outside of the function:
-let x = 0;
-let y = 0;
-let d = 50;
-let speedX = 3;
-let speedY = 2;
+
+//converting variables for ball into an object below
+const ball = {
+    x: 0,
+    y: 0,
+    d: 50,
+    speedX: 3,
+    speedY: 2,
+};
+
+
+// change these variables into an object (see above)^
+// let x = 0;
+// let y = 0;
+// let d = 50;
+// let speedX = 3;
+// let speedY = 2;
 
 // animate the circle by changing the x and y position
 // every time the animation loop invokes the draw() function:
+
+
+
+
+
+// This is a list of objects
+// The brackets indicate that it is a list of objects
+const circleData = [
+    {x: 500, y: 50, d: 100, speedX: 3, speedY: 3},
+    {x: 300, y: 150, d: 50, speedX: 2, speedY: 0},
+    {x: 400, y: 400, d: 25, speedX: 0, speedY: 1},
+    {x: 40, y: 520, d: 80, speedX: 2, speedY: 0},
+    {x: 140, y: 120, d: 150, speedX: 0, speedY: -0.5},
+    {x: 350, y: 350, d: 70, speedX: 1, speedY: 1}
+];
+//Use object functions (from const Ball) in variables
+// previously (x, y, d) now (ball.x, ball.y, ball.d)
 function draw() {
     clear();
     fill('hotpink');
-    circle(x, y, d);
-    x += speedX;
-    y += speedY;
+
+    //added counter
+    let x = 0;
+
+    //added while loop (broke it at this point)
+    // fixed by adding x++ and "const ball = circleData[x];"
+    while (x < circleData.length) {
+    const ball = circleData[x];
+    circle(ball.x, ball.y, ball.d);
+    ball.x += ball.speedX;
+    ball.y += ball.speedY;
+    x++;
+    }
+    
     drawGrid(canvasWidth, canvasHeight);
+    
 }
