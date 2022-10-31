c4eba6d4541275c17bc3585f7c3f77e5e6106435
tom <tcurran@unca.edu>
2022-10-30 21:15:48 -0400

    time

--- tutorials/tutorial10/yourtask2/sketch.js
+++ tutorials/tutorial10/yourtask2/sketch.js
@@ -1,19 +1,14 @@
-// TO DO:
-// finish warp function
-// add some keyboard events
-// periodic color change
+// TO DO: SEND IT
 // clean up unused code / improve readability
 
-
 const canvasWidth = window.innerWidth;
-const canvasHeight = window.innerHeight; 
+const canvasHeight = window.innerHeight - 10; 
 
 const face = [
     { centerX: canvasWidth, centerY: canvasHeight/2, size: 100, primaryColor: 'white',secondaryColor: 'black'},
     { centerX: canvasWidth, centerY: canvasHeight/2, size: 100, primaryColor: 'black',secondaryColor: 'white'},
     { centerX: 0 + canvasWidth, centerY: canvasHeight/2, size: 100, primaryColor: 'black',secondaryColor: 'white'},
-    { centerX: 0 + canvasWidth, centerY: canvasHeight/2, size: 100, primaryColor: 'black',secondaryColor: 'white'},
-    
+    { centerX: 0 + canvasWidth, centerY: canvasHeight/2, size: 100, primaryColor: 'black',secondaryColor: 'white'},    
 ]
 
 const nonagon = [
@@ -24,7 +19,6 @@ const nonagon = [
 
 async function setup() {
     createCanvas(canvasWidth, canvasHeight);
-
 
     for (let c = 0; c < 333; c++) {
         strokeWeight(0);
@@ -48,23 +42,73 @@ async function setup() {
                     drawLandscapeObject(x, y, radius, 7, '#002147');
                 }
     
-
     drawLandscapeObject(canvasWidth/2, canvasHeight/2, 333, 9, '#000');
-    
-    // drawLandscapeObject(canvasWidth/3, canvasHeight/3, 99, 9, '#8002d5');
-
-    // drawLandscapeObject(canvasWidth - canvasWidth/3, canvasHeight/3, 99, 9, '#E66000');
-
-    // drawLandscapeObject(canvasWidth - canvasWidth/2, canvasHeight - canvasHeight/9, 99, 9, '#FFCB00');
-
 }
-c = 0;
 
-// finish warp function
-let warp = 0
+c = 0;
+let warp = 60
+
+function mouseDragged() {
+    warp = 1 + mouseX / 10; 
+}
+function keyLog(ev) {
+    console.log(ev.code);
+    if (ev.code == 'KeyW') {
+        console.log('warpUp');
+        warp = warp + 3;
+    } else if (ev.code == 'KeyS') {
+        console.log('warpDown');
+        if (warp > 15){
+            warp = warp - 3;
+        } else if (warp < 15){
+            warp = 15 }
+    } else if (ev.code == 'ArrowUp') {
+        console.log('grow');
+        if (face[0].size<= 99)
+        face[0].size = face[0].size + 1 ;
+        if (face[1].size<= 99)
+        face[1].size = face[0].size + 1 ;
+        if (face[2].size<= 99)
+        face[2].size = face[0].size + 1 ;
+        if (face[3].size<= 99)
+        face[3].size = face[0].size + 1 ;
+    } else if (ev.code == 'ArrowDown') {
+        console.log('shrink');
+        if (face[0].size > 3)
+        face[0].size = face[0].size - 1 ;
+        if (face[1].size > 3)
+        face[1].size = face[0].size - 1 ;
+        if (face[2].size > 3)
+        face[2].size = face[0].size - 1 ;
+        if (face[3].size > 3)
+        face[3].size = face[0].size - 1 ;
+    } else if (ev.code == 'Space') {
+        width += 2;
+    } else if (ev.code == 'Escape') {
+        width -= 2;
+    } else if (ev.code == 'KeyI'){
+        drawCreature(face[0].centerX, face[0].centerY, face[0].size);
+        face[0].centerY += 5 * Math.cos(c / 9);
+        face[0].centerX += 9;
+    
+        drawCreature(face[1].centerX, face[1].centerY, face[1].size);
+        face[1].centerY += 5 * -Math.cos(c / 9);
+        face[1].centerX += 9;
+    
+        drawCreature(face[2].centerX, face[2].centerY, face[2].size);
+        face[2].centerY += 5 * -Math.cos(c / 9);
+        face[2].centerX += -9;
+        
+        drawCreature(face[3].centerX, face[3].centerY, face[3].size);
+        face[3].centerY += 5 * Math.cos(c / 9);
+        face[3].centerX += -9;
+    }
+}
+
 function draw() {
 
-    frameRate(60 + warp)
+    frameRate(warp)
+
     drawCreature(face[0].centerX, face[0].centerY, face[0].size);
     face[0].centerY += 5 * Math.sin(c / 9);
     face[0].centerX += 9;
@@ -90,20 +134,24 @@ function draw() {
     if (face[1].centerX > canvasWidth + face[1].size) {
         face[1].centerX = -face[1].size;
     }
+
     if (face[0].centerX > canvasWidth + face[0].size) {
         face[0].centerX = -face[0].size;
     }
+
     if (face[2].centerX < 0 - face[2].size) {
         face[2].centerX = 0 + canvasWidth + face[2].size;
     }
+
     if (face[3].centerX < 0 - face[3].size) {
         face[3].centerX = 0 + canvasWidth + face[3].size;
     }
+
     c++;
-    console.log(c)
 }
 
 function drawCreature(centerX, centerY, size, primaryColor='white', secondaryColor='black') {
+    
     fill('grey')
     square(centerX + size * .09, centerY + size  * .09, size * 1.02); 
     
@@ -135,8 +183,11 @@ function drawCreature(centerX, centerY, size, primaryColor='white', secondaryCol
 }
 
 function drawLandscapeObject(x, y, radius, npoints, fillColor="white") {
+    
     let angle = TWO_PI / npoints;
+    
     fill(fillColor)
+    
     beginShape();
     for (let a = 0; a < TWO_PI; a += angle) {
       let sx = x + cos(a) * radius;
@@ -146,3 +197,5 @@ function drawLandscapeObject(x, y, radius, npoints, fillColor="white") {
     }
     endShape(CLOSE);
 }
+
+document.addEventListener('keydown', keyLog);
