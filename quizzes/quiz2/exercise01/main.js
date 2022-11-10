5bd625bf7c645fac4d90c20011040458e70ed1ce
tom <tcurran@unca.edu>
2022-11-10 13:01:33 -0500

    added quiz 2

---
+++ quizzes/quiz2/exercise01/main.js
@@ -0,0 +1,20 @@
+function showFox() {
+    // your code here...
+    console.log('Change image and paragraph to fox...');
+}
+
+function showLion() {
+    // your code here...
+    console.log('Change image and paragraph to lion...');
+}
+
+function showTiger() {
+    // your code here...
+    console.log('Change image and paragraph to tiger...');
+}
+
+function showZebra() {
+    // your code here...
+    console.log('Change image and paragraph to zebra...');
+}
+
